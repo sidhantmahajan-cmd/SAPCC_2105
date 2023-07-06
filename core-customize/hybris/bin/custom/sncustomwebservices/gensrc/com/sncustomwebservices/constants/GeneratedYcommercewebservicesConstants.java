606bdc76bd1dc8a3bc53dc16bbac16205d74d3ba
Anand Mund <anand.mund1@bristlecone.com>
2023-07-06 22:02:29 +0530

    adding 2211 files
    adding 2211 files.

---
+++ core-customize/hybris/bin/custom/sncustomwebservices/gensrc/com/sncustomwebservices/constants/GeneratedYcommercewebservicesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 23-Jun-2023, 10:39:59 pm                    ---
+ * ----------------------------------------------------------------
+ */
+package com.sncustomwebservices.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYcommercewebservicesConstants
+{
+	public static final String EXTENSIONNAME = "sncustomwebservices";
+	
+	protected GeneratedYcommercewebservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ core-customize/hybris/bin/custom/spartacussampledata/gensrc/de/hybris/platform/spartacussampledata/constants/GeneratedSpartacussampledataConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 23-Jun-2023, 10:39:59 pm                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.spartacussampledata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedSpartacussampledataConstants
+{
+	public static final String EXTENSIONNAME = "spartacussampledata";
+	
+	protected GeneratedSpartacussampledataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
